d77ddca12cae2e275f957d6b45e1f0273baa1dc2
Pierre G <phg17@ic.ac.uk>
2022-01-24 11:22:15 +0000

    Update Demo_TRF.py
    Just a few comments

--- demos/Demo_TRF.py
+++ demos/Demo_TRF.py
@@ -20,23 +20,28 @@ def get_raw_info():
     raw.drop_channels(['Sound', 'Diode', 'Button'])
     return raw.info
 
-
+#Get Topology Info
 info = get_raw_info()
 info['sfreq'] = Fs
 
+#Load Data
 envelope = np.load(join(path_data, 'envelope_1.npy'))
 eeg = np.load(join(path_data, 'eeg_1.npy'))
 vowels = np.load(join(path_data, 'vowels_1.npy'))
 
+#Create Features matrix 
 xtrf = np.hstack([envelope, vowels])
 ytrf = eeg[:]  # Make a deep copy?
 
+#Fit
 trf = TRFEstimator(tmin=-.5, tmax=.5, srate=Fs, alpha=[10])
 trf.fit_from_cov(xtrf, ytrf, part_length=60, clear_after=False)
 
+#Extract Coef
 coef_envelope = trf.get_coef()[:, 0, :, 0].T
 coef_vowels = trf.get_coef()[:, 1, :, 0].T
 
+#Plot using mne 
 ev1 = mne.EvokedArray(coef_envelope, info, tmin=trf.tmin)
 ev2 = mne.EvokedArray(coef_vowels, info, tmin=trf.tmin)
 
